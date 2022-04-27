48fc4900a88c815b0535fd2b168c0fce854f4b1b
maaaaakoto35 <fujiwaramakoto@smartcamp.co.jp>
2022-04-27 20:28:08 +0900

    [FIXME] type error at crud.py

--- backend/app/api/crud.py
+++ backend/app/api/crud.py
@@ -1,3 +1,4 @@
+from ctypes import Union
 from app.models.pydantic import SummaryPayLoadSchema
 from app.models.tortoise import TextSummary
 from typing import List
@@ -10,5 +11,25 @@ async def post(payload: SummaryPayLoadSchema) -> int:
     return summary.id
 
 async def get_all() -> List:
-    summarys = await TextSummary.all().values()
-    return summarys
+    summariess = await TextSummary.all().values()
+    return summariess
+
+async def get(id: int) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).first().values()
+    if summary:
+        return summary[0]
+    return None
+
+async def put(id: int, payload: SummaryPayLoadSchema) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).update(
+        summary=payload.summary
+    )
+
+    if summary:
+        update_summary = await TextSummary.filter(id=id).first().values()
+        return update_summary[0]
+    return None
+
+async def delete(id: int) -> int:
+    summary = await TextSummary.filter(id=id).delete()
+    return summary
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,9 @@
+from http.client import HTTPException
 import logging
 import os
-from urllib import response
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 
 from app.api import crud
@@ -14,6 +15,19 @@ log = logging.getLogger("uvicorn")
 
 app = FastAPI()
 
+origins = [
+    "*",
+    "localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 @app.on_event("startup")
 async def startup_event():
     log.info("Starting up ...")
@@ -22,6 +36,10 @@ async def startup_event():
 @app.on_event("shutdown")
 async def shutdown_event():
     log.info("Shutting down...")
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World!"}
 
 @app.post("/summary", response_model = SummaryResponseSchema, status_code=201)
 async def create_summary(payload: SummaryPayLoadSchema) -> SummaryResponseSchema:
@@ -37,6 +55,26 @@ async def create_summary(payload: SummaryPayLoadSchema) -> SummaryResponseSchema
 async def read_all_summaries() -> List[SummarySchema]:
     return await crud.get_all()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World!"}
+@app.get("/summay/{id}", response_model=SummaryResponseSchema)
+async def get_summary(id: int = Path(..., gt=0)) -> SummaryResponseSchema:
+    summary = await crud.get(id)
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
+    return summary
+
+
+@app.put("/summary/{id}", response_model=SummarySchema)
+async def update_summary(payload: SummaryPayLoadSchema, id: int = Path(..., gt=0)) -> SummarySchema:
+    summary = await crud.put(id, payload)
+
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
+    return summary
+
+@app.delete("/summay/{id}", response_model=SummaryResponseSchema)
+async def delete_summary(id: int = Path(..., gt=0)) -> SummaryResponseSchema:
+    summary = await crud.get(id)
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
+    await crud.delete(id)
+    return summary
